b5792a8080ddfb478aa33954dc4d1ba9151d5049
njb <njblondhe@gmail.com>
2019-03-27 19:36:28 -0500

    UI updates to playlist

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { SpotifyService } from '../spotify.service';
+import { anchorDef } from '@angular/core/src/view';
 
 @Component({
   selector: 'app-playlist',
@@ -10,9 +11,11 @@ export class PlaylistComponent implements OnInit {
   private token;
   error;
   @ViewChild('recommended') recommended;
+  @ViewChild('collapseAnchor') collapseAnchor;
   spotifyUser: string;
   savedTracks = [];
   recommendations = [];
+  recStore = {};
   currentTrack = {};
 
   constructor(private spotifyService: SpotifyService) { }
@@ -28,7 +31,7 @@ export class PlaylistComponent implements OnInit {
 
   getSavedTracks() {
     this.spotifyService.getSavedTracks(this.token).subscribe(data => {
-      console.log(data['items']);
+      // console.log(data['items']);
       this.savedTracks = data['items'].map(track => (
         {
           artists: track['track']['artists'],
@@ -47,16 +50,16 @@ export class PlaylistComponent implements OnInit {
       }
     },
     () => {
-      // this.savedTracks.forEach(element => {
-      //   this.getRecommendations(element.id);
-      // });
-      this.getRecommendations(this.savedTracks[0]['id']);
+      this.savedTracks.forEach(element => {
+        this.getRecommendations(element.id);
+      });
+      // console.log(this.recStore);
     }
     );
   }
   getRecommendations(id) {
     this.spotifyService.getRecommendations(id, this.token).subscribe(data => {
-      this.recommendations = data['tracks'].map(track => (
+      this.recStore[id] = data['tracks'].map(track => (
         {
           artists: track['artists'],
           track: track['name'],
@@ -64,36 +67,53 @@ export class PlaylistComponent implements OnInit {
           coverLow: track['album']['images'][2]['url'],
           coverHigh: track['album']['images'][0]['url'],
           seedId: id,
-          id: track['id']
+          // id: track['id']
         }
       ));
-      this.savedTracks[0]['recs'] = this.recommendations;
-      console.log(this.savedTracks[0]);
+      // this.savedTracks[0]['recs'] = this.recommendations;
+      // console.log(this.savedTracks[0]);
     });
   }
 
-  toggleRecommendation(event, i) {
-    console.log('list index', i);
-    // const element = event.target;
-    // console.log(element);
-    // element.classList.toggle('active');
+  toggleRecommendation(event) {
+    let anchor = event.target.parentNode.parentNode;
+    const tracksList = anchor.parentNode;
+    const allSavedChildren = anchor.parentNode.parentNode.children;
 
-    // if (this.savedTracks[index].isActive) {
-    //   this.savedTracks[index].isActive = false;
-    // } else {
-    //   this.savedTracks[index].isActive = true;
-    // }
-    const panel = this.recommended.nativeElement;
-    if (panel.style.display === 'flex') {
-      panel.style.display = 'none';
+    if (anchor.classList.contains('highlighted')) {
+      anchor.classList.remove('highlighted');
     } else {
-      panel.style.display = 'flex';
+      anchor.classList.add('highlighted');
+    }
+
+    for (let j = 0; j < allSavedChildren.length; j++) {
+      const current = allSavedChildren[j];
+
+      // Mute not selected
+      if (current !== tracksList) {
+        if (current.classList.contains('mute')) {
+           current.classList.remove('mute');
+        } else {
+           current.classList.add('mute');
+        }
+      }
+    }
+    for (let j = 0; j < 5; j++) {
+      const panel = anchor.nextElementSibling;
+      // Display
+      if (panel.style.display === 'flex') {
+        panel.style.display = 'none';
+      } else {
+        panel.style.display = 'flex';
+      }
+
+      // Ease drop down
+      if (panel.style.maxHeight){
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + 'px';
+      } 
+      anchor = panel;
     }
   }
-
-  updateCurrentTrack(i) {
-    this.currentTrack = this.savedTracks[i];
-    // this.currentTrack['cover'] = this.savedTracks[i].coverHigh;
-  }
-
 }
